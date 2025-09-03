c7b1d68eccd19f092abd22cefd3ba31f6da9d1f1
Krishan Sharma <krishansharma17022004@gmail.com>
2025-09-03 15:47:33 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import dill
 
 import numpy as np 
 import pandas as pd 
+from sklearn.metrics import r2_score
 
 
 
@@ -20,3 +21,50 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+# def evaluate_model(X_train,y_train,X_test,y_test,models):
+#     try:
+#         report = {}
+
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+
+#             model.fit(X_train,y_train) # Train Model
+
+#             y_train_pred = model.predict(X_train)
+
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(y_train,y_train_pred)
+
+#             test_model_score = r2_score(y_test,y_test_pred)
+
+#             report[list(model.keys())[i]] = test_model_score
+
+#             return report
+#     except Exception as e:
+#         raise CustomException(e,sys)
+    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+        try:
+            report = {}
+
+            for name, model in models.items():
+                model.fit(X_train, y_train)  # Train model
+
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
+
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
+
+                report[name] = test_model_score  # Use model name as key
+
+            return report  # Return after all models are evaluated
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
